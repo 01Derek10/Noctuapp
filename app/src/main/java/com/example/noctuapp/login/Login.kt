00f705b438c4fa42f8f2e528f69fae2b745d6d2a
01Derek10 <131251445+01Derek10@users.noreply.github.com>
2024-05-23 19:23:49 +0200

    .

--- app/src/main/java/com/example/noctuapp/login/Login.kt
+++ app/src/main/java/com/example/noctuapp/login/Login.kt
@@ -1,47 +1,21 @@
 package com.example.noctuapp.login
 
-
 import android.util.Log
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.KeyboardActions
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Visibility
 import androidx.compose.material.icons.filled.VisibilityOff
-import androidx.compose.material3.AlertDialog
-import androidx.compose.material3.Button
-import androidx.compose.material3.ButtonDefaults
-import androidx.compose.material3.CircularProgressIndicator
-import androidx.compose.material3.FloatingActionButton
-import androidx.compose.material3.FloatingActionButtonDefaults
-import androidx.compose.material3.Icon
-import androidx.compose.material3.IconButton
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.material3.*
+import androidx.compose.runtime.*
 import androidx.compose.runtime.saveable.rememberSaveable
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.focus.FocusDirection
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalFocusManager
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
@@ -53,27 +27,30 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.window.Dialog
 import androidx.navigation.NavController
+import androidx.navigation.compose.rememberNavController
 import com.example.myapplication.navegation.selectNavegation
 import com.example.noctuapp.R
 import com.example.noctuapp.elements.TransparentTextField
 import com.example.noctuapp.ui.theme.NoctuappTheme
+import com.example.noctuapp.ui.theme.noctuapp
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
-
+import kotlinx.coroutines.withContext
+import java.io.OutputStreamWriter
+import java.net.HttpURLConnection
+import java.net.URL
 
 @Composable
 fun VistaLogin(navController: NavController) {
-
     val username = rememberSaveable { mutableStateOf("") }
     val password = rememberSaveable { mutableStateOf("") }
     var passwordVisibility by remember { mutableStateOf(false) }
     var datosError by remember { mutableStateOf(false) }
     val focusManager = LocalFocusManager.current
-    var containerColorUser by remember { mutableStateOf(Color.Black) }
-    var containerColorPass by remember { mutableStateOf(Color.Black) }
-    val context = LocalContext.current
+    var containerColorUser by remember { mutableStateOf(noctuapp) }
+    var containerColorPass by remember { mutableStateOf(noctuapp) }
     val coroutineScope = rememberCoroutineScope()
     var showProgressDialog by remember { mutableStateOf(false) }
-
 
     if (showProgressDialog) {
         Dialog(onDismissRequest = { showProgressDialog = false }) {
@@ -83,7 +60,63 @@ fun VistaLogin(navController: NavController) {
                     .background(Color.Transparent, shape = RoundedCornerShape(8.dp))
                     .padding(16.dp)
             ) {
-                CircularProgressIndicator(color = Color.Black)
+                CircularProgressIndicator(color = Color(0xFF800080)) // Color morado
+            }
+        }
+    }
+
+    suspend fun checkCredentials(username: String, password: String): Boolean {
+        return withContext(Dispatchers.IO) {
+            try {
+                val url = URL("http://192.168.215.190/login.php")
+                val connection = url.openConnection() as HttpURLConnection
+                connection.requestMethod = "POST"
+                connection.doOutput = true
+                connection.setRequestProperty("Content-Type", "application/x-www-form-urlencoded")
+
+                val requestBody = "username=$username&password=$password"
+                val outputStreamWriter = OutputStreamWriter(connection.outputStream)
+                outputStreamWriter.write(requestBody)
+                outputStreamWriter.flush()
+
+                val responseCode = connection.responseCode
+                val responseMessage = connection.inputStream.bufferedReader().readText()
+                connection.disconnect()
+
+                Log.d("HTTP_RESPONSE", "Response Code: $responseCode, Response: $responseMessage")
+
+                responseCode == 200 && responseMessage.contains("\"status\":\"success\"")
+            } catch (e: Exception) {
+                Log.e("HTTP_ERROR", "Error during HTTP request: ${e.message}")
+                false
+            }
+        }
+    }
+
+    suspend fun registerUser(username: String, password: String): Boolean {
+        return withContext(Dispatchers.IO) {
+            try {
+                val url = URL("http://192.168.215.190/register.php")
+                val connection = url.openConnection() as HttpURLConnection
+                connection.requestMethod = "POST"
+                connection.doOutput = true
+                connection.setRequestProperty("Content-Type", "application/x-www-form-urlencoded")
+
+                val requestBody = "username=$username&password=$password"
+                val outputStreamWriter = OutputStreamWriter(connection.outputStream)
+                outputStreamWriter.write(requestBody)
+                outputStreamWriter.flush()
+
+                val responseCode = connection.responseCode
+                val responseMessage = connection.inputStream.bufferedReader().readText()
+                connection.disconnect()
+
+                Log.d("HTTP_RESPONSE", "Response Code: $responseCode, Response: $responseMessage")
+
+                responseCode == 200 && responseMessage.contains("\"status\":\"success\"")
+            } catch (e: Exception) {
+                Log.e("HTTP_ERROR", "Error during HTTP request: ${e.message}")
+                false
             }
         }
     }
@@ -103,7 +136,11 @@ fun VistaLogin(navController: NavController) {
             horizontalArrangement = Arrangement.Center,
             verticalAlignment = Alignment.CenterVertically
         ) {
-            Image(painter = painterResource(id = R.drawable.ic_launcher_foreground), contentDescription ="logo", modifier = Modifier.size(200.dp) )
+            Image(
+                painter = painterResource(id = R.drawable.logo_con_nombre),
+                contentDescription = "logo",
+                modifier = Modifier.size(200.dp)
+            )
         }
         TransparentTextField(
             textFieldValue = username,
@@ -161,7 +198,7 @@ fun VistaLogin(navController: NavController) {
                     .width(280.dp)
                     .height(50.dp),
                 shape = RoundedCornerShape(50),
-                colors =ButtonDefaults.buttonColors(Color.Black),
+                colors = ButtonDefaults.buttonColors(noctuapp),
                 onClick = {
                     coroutineScope.launch {
                         try {
@@ -171,22 +208,25 @@ fun VistaLogin(navController: NavController) {
                                 containerColorUser = Color(0xFFFE0000)
                                 formIsValid = false
                             } else {
-                                containerColorUser = Color.Black
+                                containerColorUser = noctuapp
                             }
 
                             if (password.value.isEmpty()) {
                                 containerColorPass = Color(0xFFFE0000)
                                 formIsValid = false
                             } else {
-                                containerColorPass = Color.Black
+                                containerColorPass = noctuapp
                             }
 
                             if (formIsValid) {
-                                // aqui tendria que hacer la llamada a la comprobacion en la bbdd,
-                                //SI LOS DATOS SON CORRECTOS TE HACE LA NAVEGACION A LA SIGUEINTE PESTAÑA SINO SALTA EL ERROR
-                                Log.i("login","todo bien")
-                                navController.navigate(route = selectNavegation.Lugares.route)
-
+                                showProgressDialog = true
+                                val success = checkCredentials(username.value, password.value)
+                                showProgressDialog = false
+                                if (success) {
+                                    navController.navigate(route = selectNavegation.Lugares.route)
+                                } else {
+                                    datosError = true
+                                }
                             }
                         } catch (e: Exception) {
                             Log.e("ButtonOnClick", "Error al realizar el login: ${e.message}")
@@ -194,28 +234,76 @@ fun VistaLogin(navController: NavController) {
                         }
                     }
                 }
-
-            ){
+            ) {
                 Text(
                     text = stringResource(id = R.string.login),
                     style = MaterialTheme.typography.headlineSmall
                 )
             }
+            Spacer(modifier = Modifier.size(10.dp))
+
+            Button(
+                modifier = Modifier
+                    .width(280.dp)
+                    .height(50.dp),
+                shape = RoundedCornerShape(50),
+                colors = ButtonDefaults.buttonColors(noctuapp),
+                onClick = {
+                    coroutineScope.launch {
+                        try {
+                            var formIsValid = true
+
+                            if (username.value.isEmpty()) {
+                                containerColorUser = Color(0xFFFE0000)
+                                formIsValid = false
+                            } else {
+                                containerColorUser = noctuapp
+                            }
+
+                            if (password.value.isEmpty()) {
+                                containerColorPass = Color(0xFFFE0000)
+                                formIsValid = false
+                            } else {
+                                containerColorPass = noctuapp
+                            }
+
+                            if (formIsValid) {
+                                showProgressDialog = true
+                                val success = registerUser(username.value, password.value)
+                                showProgressDialog = false
+                                if (success) {
+                                    navController.navigate(route = selectNavegation.Lugares.route)
+                                } else {
+                                    datosError = true
+                                }
+                            }
+                        } catch (e: Exception) {
+                            Log.e("ButtonOnClick", "Error al realizar el registro: ${e.message}")
+                            datosError = true // Mostrar el diálogo de error si ocurre una excepción
+                        }
+                    }
+                }
+            ) {
+                Text(
+                    text = stringResource(id = R.string.register),
+                    style = MaterialTheme.typography.headlineSmall
+                )
+            }
         }
     }
-    if(datosError){
+    if (datosError) {
         AlertDialog(
             onDismissRequest = {
-                datosError=false
+                datosError = false
             },
             confirmButton = {
                 Button(
                     colors = ButtonDefaults.buttonColors(
-                        containerColor = Color.Black
+                        containerColor = noctuapp
                     ),
                     onClick = {
                         // Acción para el botón de confirmar
-                        datosError=false
+                        datosError = false
                     }
                 ) {
                     Text("Salir")
@@ -230,164 +318,23 @@ fun VistaLogin(navController: NavController) {
         )
     }
 
-
     Column(
         modifier = Modifier
             .fillMaxSize()
             .padding(horizontal = 16.dp),
         horizontalAlignment = Alignment.End,
         verticalArrangement = Arrangement.Bottom
-
     ) {
 
     }
 }
 
-
-
-
-@Preview(showBackground = true, showSystemUi = true)
+@Preview(showBackground = true)
 @Composable
-fun VistaLoginPreview() {
+fun PreviewVistaLogin() {
     NoctuappTheme {
-        val username = rememberSaveable { mutableStateOf("") }
-        val password = rememberSaveable { mutableStateOf("") }
-        var passwordVisibility by remember { mutableStateOf(false) }
-        val focusManager = LocalFocusManager.current
-        var containerColorUser by remember { mutableStateOf(Color.Black) }
-        var containerColorPass by remember { mutableStateOf(Color.Black) }
-
-
-
-        Column(
-            modifier = Modifier
-                .fillMaxWidth()
-                .padding(horizontal = 16.dp),
-            horizontalAlignment = Alignment.CenterHorizontally,
-            verticalArrangement = Arrangement.spacedBy(8.dp)
-
-        ) {
-            Row(
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .padding(vertical = 10.dp),
-                horizontalArrangement = Arrangement.Center,
-                verticalAlignment = Alignment.CenterVertically
-            ) {
-                Image(
-                    painter = painterResource(id = R.drawable.ic_launcher_foreground),
-                    contentDescription = "logo",
-                    modifier = Modifier.size(200.dp)
-                )
-            }
-            Spacer(modifier = Modifier.size(60.dp))
-
-            TransparentTextField(
-                textFieldValue = username,
-                textLabel = stringResource(R.string.usuario),
-                keyboardType = KeyboardType.Email,
-                keyboardActions = KeyboardActions(
-                    onNext = {
-                        focusManager.moveFocus(FocusDirection.Down)
-                    }
-                ),
-                imeAction = ImeAction.Next,
-                containerColor = containerColorUser
-
-            )
-            TransparentTextField(
-                textFieldValue = password,
-                textLabel = stringResource(R.string.contraseña),
-                keyboardType = KeyboardType.Password,
-                keyboardActions = KeyboardActions(
-                    onNext = {
-                        focusManager.clearFocus()
-                    }
-                ),
-                imeAction = ImeAction.Done,
-                containerColor = containerColorPass,
-                trailingIcon = {
-                    IconButton(
-                        onClick = {
-                            passwordVisibility = !passwordVisibility
-                        }
-                    ) {
-                        Icon(
-                            imageVector = if (passwordVisibility) {
-                                Icons.Default.Visibility
-                            } else {
-                                Icons.Default.VisibilityOff
-                            },
-                            contentDescription = "Ocultar contraseña"
-                        )
-                    }
-                },
-                visualTransformation = if (passwordVisibility) {
-                    VisualTransformation.None
-                } else {
-                    PasswordVisualTransformation()
-                }
-            )
-
-
-            Spacer(modifier = Modifier.size(10.dp))
-
-            Column(
-                modifier = Modifier.fillMaxWidth(),
-                horizontalAlignment = Alignment.CenterHorizontally,
-                verticalArrangement = Arrangement.spacedBy(8.dp)
-            ) {
-                Button(
-                    modifier = Modifier
-                        .width(280.dp)
-                        .height(50.dp),
-                    shape = RoundedCornerShape(50),
-                    colors = ButtonDefaults.buttonColors(Color.Black),
-                    onClick = {
-                        //Logica boton
-                        var formIsValid = true
-
-                        if (username.value.isEmpty()) {
-                            containerColorUser = Color(0xFF8B0000)
-                            formIsValid = false
-                        } else {
-                            containerColorUser = Color.Black
-                        }
-                        if (password.value.isEmpty()) {
-                            containerColorPass = Color(0xFF8B0000)
-                            formIsValid = false
-                        } else {
-                            containerColorPass = Color.Black
-                        }
-
-                        if (formIsValid) {
-
-
-
-                        }
-                    }
-
-                ) {
-                    Text(
-                        text = stringResource(id = R.string.login),
-                        style = MaterialTheme.typography.headlineSmall
-                    )
-                }
-            }
-
-
-        }
-        Column(
-            modifier = Modifier
-                .fillMaxSize()
-                .padding(horizontal = 16.dp),
-            horizontalAlignment = Alignment.End,
-            verticalArrangement = Arrangement.Bottom
-
-        ) {
-             {
-
-            }
-        }
+        // Simulamos un NavController
+        val navController = rememberNavController()
+        VistaLogin(navController = navController)
     }
 }
--- app/src/main/java/com/example/noctuapp/lugares/Lugares.kt
+++ app/src/main/java/com/example/noctuapp/lugares/Lugares.kt
@@ -2,15 +2,27 @@ package com.example.noctuapp.lugares
 
 
 import android.annotation.SuppressLint
-import androidx.compose.foundation.layout.Arrangement
+
 import androidx.compose.foundation.layout.Column
+
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.CornerSize
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.MaterialTheme
+
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
+
 import com.example.noctuapp.ui.theme.NoctuappTheme
 
 
@@ -18,25 +30,116 @@ import com.example.noctuapp.ui.theme.NoctuappTheme
 @SuppressLint("RestrictedApi")
 @Composable
 fun VistaLugares(navController: NavController) {
+    NoctuappTheme {
+        val listPlaces = listOf(
+            PartyPlace(1, "Sala Gold", "Calle Luis de Velázquez, 5"),
+            PartyPlace(2, "Theatro Club", "Calle Lazcano, 5"),
+            PartyPlace(3, "Andén", "Plaza de Uncibay, 8"),
+            PartyPlace(4, "Discoteca Liceo", "Calle Beatas, 21"),
+            PartyPlace(5, "Sala Wenge", "Calle Santa Lucía, 11"),
+            PartyPlace(6, "Velvet Club", "Calle Convalecientes, 11"),
+            PartyPlace(7, "Bubbles Lounge Club", "Calle Juan de Padilla, 18"),
+            PartyPlace(8, "Antigua Casa de Guardia", "Alameda Principal, 18"),
+            PartyPlace(9, "ZZ Pub", "Calle Tejón y Rodríguez, 6"),
+            PartyPlace(10, "La Botellita", "Calle Luis de Velázquez, 3"),
+            PartyPlace(11, "Clarence Jazz Club", "Calle Cañón, 5"),
+            PartyPlace(12, "Sala Premier", "Calle Molina Lario, 2"),
+            PartyPlace(13, "Malafama", "Calle Comedias, 15"),
+            PartyPlace(14, "Sala White", "Calle José Denis Belgrano, 3"),
+            PartyPlace(15, "The Hall", "Calle Héroe de Sostoa, 65"),
+        )
+        @Composable
+        fun PlaceItem(place: PartyPlace) {
+            Card (
+                modifier = Modifier
+                    .padding(horizontal = 8.dp, vertical = 8.dp)
+                    .fillMaxWidth(),
+                elevation = CardDefaults.cardElevation(defaultElevation = 2.dp),
+                colors = CardDefaults.cardColors(containerColor = MaterialTheme.colorScheme.primary),
+                shape = RoundedCornerShape(corner = CornerSize(16.dp))
 
+            ){
+                Column(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(16.dp)
+                ) {
+                    Text(text = place.name, style = MaterialTheme.typography.headlineLarge)
+                    Text(text = "Ubicación: ${place.location}", style = MaterialTheme.typography.bodyMedium)
+                }
+            }
+
+        }
+        LazyColumn {
+            items(
+                items = listPlaces,
+                itemContent = { place ->
+                    PlaceItem(place = place)
+                }
+            )
+        }
+    }
 }
-
-
+data class PartyPlace(
+    val id: Int,
+    val name: String,
+    val location: String
+)
 
 
 @Preview(showBackground = true, showSystemUi = true)
 @Composable
 fun VistaLoginPreview() {
     NoctuappTheme {
+        val listPlaces = listOf(
+            PartyPlace(1, "Sala Gold", "Calle Luis de Velázquez, 5"),
+            PartyPlace(2, "Theatro Club", "Calle Lazcano, 5"),
+            PartyPlace(3, "Andén", "Plaza de Uncibay, 8"),
+            PartyPlace(4, "Discoteca Liceo", "Calle Beatas, 21"),
+            PartyPlace(5, "Sala Wenge", "Calle Santa Lucía, 11"),
+            PartyPlace(6, "Velvet Club", "Calle Convalecientes, 11"),
+            PartyPlace(7, "Bubbles Lounge Club", "Calle Juan de Padilla, 18"),
+            PartyPlace(8, "Antigua Casa de Guardia", "Alameda Principal, 18"),
+            PartyPlace(9, "ZZ Pub", "Calle Tejón y Rodríguez, 6"),
+            PartyPlace(10, "La Botellita", "Calle Luis de Velázquez, 3"),
+            PartyPlace(11, "Clarence Jazz Club", "Calle Cañón, 5"),
+            PartyPlace(12, "Sala Premier", "Calle Molina Lario, 2"),
+            PartyPlace(13, "Malafama", "Calle Comedias, 15"),
+            PartyPlace(14, "Sala White", "Calle José Denis Belgrano, 3"),
+            PartyPlace(15, "The Hall", "Calle Héroe de Sostoa, 65"),
 
-            Column(
-                modifier = Modifier.fillMaxWidth(),
-                horizontalAlignment = Alignment.CenterHorizontally,
-                verticalArrangement = Arrangement.spacedBy(8.dp)
-            ) {
+        )
+        @Composable
+        fun PlaceItem(place: PartyPlace) {
+            Card (
+                modifier = Modifier
+                    .padding(horizontal = 8.dp, vertical = 8.dp)
+                    .fillMaxWidth(),
+                elevation = CardDefaults.cardElevation(defaultElevation = 2.dp),
+                colors = CardDefaults.cardColors(containerColor = MaterialTheme.colorScheme.primary),
+                shape = RoundedCornerShape(corner = CornerSize(16.dp))
 
+            ){
+                Column(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(16.dp)
+                ) {
+                    Text(text = place.name, style = MaterialTheme.typography.headlineLarge)
+                    Text(text = "Ubicación: ${place.location}", style = MaterialTheme.typography.bodyMedium)
+                }
             }
 
         }
+        LazyColumn {
+            items(
+                items = listPlaces,
+                itemContent = { place ->
+                    PlaceItem(place = place)
+                }
+            )
+        }
 
     }
+
+        }
